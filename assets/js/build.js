08c129520560759976b3cdf41d3975777c4ba8ec
Jimmy7610 <eliassonjimmy76@gmail.com>
2026-05-13 02:02:27 +0200

    Add robots and sitemap (Build 96)

--- assets/js/build.js
+++ assets/js/build.js
@@ -1,5 +1,5 @@
 // INSTÄLLNING - Öka detta nummer med 1 innan varje push till main, så Jimmy kan se att live-sidan visar rätt version.
-window.TAREN_BUILD_NUMBER = 95;
+window.TAREN_BUILD_NUMBER = 96;
 
 document.addEventListener('DOMContentLoaded', () => {
     const badge = document.createElement('div');
